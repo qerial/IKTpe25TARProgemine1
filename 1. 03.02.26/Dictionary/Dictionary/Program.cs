23e0fda1004378aeffc7228cfb5d847a0efa694f
Opilane TTHK <opilane@tthk.ee>
2026-02-03 15:16:21 +0200

    code

---
+++ 1. 03.02.26/Dictionary/Dictionary/Program.cs
@@ -0,0 +1,28 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Dictionary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Dictionary ja foreach");
+            int numberIndex = 1;
+            // var on muutuja
+            var domains = new Dictionary<string, string>()
+            {
+            {"fi", "Finland"},
+            {"se", "Sweden"},
+            {"de", "Germany"},
+            {"fr", "France"},
+            {"es", "Spain"}
+            };
+            //kasutate foreachi ja juurde tuleb lisada kolmas rida
+            foreach (var domain in domains)
+            {
+                Console.WriteLine($" {domain.Key} - {domain.Value} - {numberIndex}");
+                numberIndex++;
+            }
+        }
+    }
+}
