0339b898bedd854ccffdd440810b2e88ae20a4a1
Opilane TTHK <opilane@tthk.ee>
2026-02-06 10:05:28 +0200

    h

---
+++ 2. 06.02.26/RegEx/RegEx/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.RegularExpressions;
+
+namespace RegEx
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Teeme Regular expression harjutuse");
+
+            string word = "#CD5C5C";
+            Console.WriteLine("Hex code: " + word);
+            Console.WriteLine("Kas on regex: " + RegExTest(word));
+        }
+
+        public static bool RegExTest(string word)
+        {
+            //Regular Expression kontrollib, kas sisestav string
+            //vastab nõuetele
+            return Regex.IsMatch(word, @"[#][0-9A-Fa-f]{6}\b");
+
+        }
+    }
+}
