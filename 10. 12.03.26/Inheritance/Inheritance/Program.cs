cb3ef3592d5e5938dedf30df764d92085bfa6590
Opilane TTHK <opilane@tthk.ee>
2026-03-12 11:06:37 +0200

    new code

---
+++ 10. 12.03.26/Inheritance/Inheritance/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Runtime.ExceptionServices;
+
+namespace Inheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insert to numbers: ");
+            Console.WriteLine("First number: ");
+            int firstNr = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Second number: ");
+            int secondNr = Convert.ToInt32(Console.ReadLine());
+
+            Rectangle rectangle = new Rectangle();
+            rectangle.SetWidth(firstNr);
+            rectangle.SetHeight(secondNr);
+
+            Console.WriteLine("Total area: {0}", rectangle.GetArea);
+
+        }
+    }
+    class Shape 
+    {
+        public void SetWidth(int w)
+        {
+            width = w;
+        }
+        public void SetHeight(int h)
+        {
+            width = h;
+        }
+
+        protected int width;
+        protected int height;
+    }
+    //koolon tähendab pärimist
+    class Rectangle : Shape
+    {
+        public int GetArea()
+        {
+            //return tagasitab info selles meetodis toimunud loogika kohta
+            return (width + height);
+        }
+
+    }
+}
