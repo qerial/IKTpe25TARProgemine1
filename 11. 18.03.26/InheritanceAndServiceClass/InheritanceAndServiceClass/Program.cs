71feca879eead99a7efce25eb70fa60fedf0b51f
Opilane TTHK <opilane@tthk.ee>
2026-03-18 11:33:23 +0200

    update

---
+++ 11. 18.03.26/InheritanceAndServiceClass/InheritanceAndServiceClass/Program.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Hosting;
+
+namespace IncheritanceAndServicesClass.Appservices
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
