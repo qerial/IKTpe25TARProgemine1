6eb5ec96a2f697ca46c2e82b42c3210e841eefcb
Opilane TTHK <opilane@tthk.ee>
2026-03-26 10:38:45 +0200

    t

---
+++ suvalised asjad/Sonatagurpidi/Sonatagurpidi/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Sonatagurpidi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Vali mida soovid testida");
+            Console.WriteLine("1.Sõnad tagurpidi.");
+            Console.WriteLine("\n");
+            int choice = int.Parse(Console.ReadLine());
+            switch (choice)
+            {
+                default:
+                    choice = 1;
+                    sonatagurpidi();
+                    break;
+            }
+            static void sonatagurpidi()
+            {
+                Console.Clear();
+                string word = Console.ReadLine();
+
+                char[] chars = word.ToCharArray();
+                Array.Reverse(chars);
+                string reversed = new string(chars);
+
+                Console.WriteLine(reversed);
+            }
+        }
+    }
+}
