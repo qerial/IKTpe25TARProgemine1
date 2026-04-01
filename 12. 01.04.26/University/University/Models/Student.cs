90135783d5be5ca7fe7a2a350545760f2eb3c407
Opilane TTHK <opilane@tthk.ee>
2026-04-01 11:31:49 +0300

    update

---
+++ 12. 01.04.26/University/University/Models/Student.cs
@@ -0,0 +1,10 @@
+﻿namespace University.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string LastName { get; set; }
+        public string FirstMidName { get; set; }
+        public DateTime Enrollmentdate { get; set; }
+    }
+}
