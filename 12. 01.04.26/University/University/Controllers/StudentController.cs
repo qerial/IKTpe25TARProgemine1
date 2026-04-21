8b2f831cac755329c0fe2a4637ef0a21ab6cca08
Opilane TTHK <opilane@tthk.ee>
2026-04-21 14:05:45 +0300

    new update added create and view details

--- 12. 01.04.26/University/University/Controllers/StudentController.cs
+++ 12. 01.04.26/University/University/Controllers/StudentController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
 using University.Data;
+using University.ViewModel;
 
 namespace University.Controllers
 {
@@ -45,6 +47,15 @@ namespace University.Controllers
             //leiame studenti Id järgi
             var student = await _context.Students
                 .FirstOrDefaultAsync(m => m.Id == id);
+
+
+            var vm = new ViewModel.StudentDetailsViewModel
+            {
+                Id = student.Id,
+                LastName = student.LastName,
+                FirstMidName = student.FirstMidName,
+                EnrollmentDate = student.EnrollmentDate
+            };
             //kui student on null, siis tagastame NotFound() tulemuse
 
             if (student == null)
@@ -52,7 +63,39 @@ namespace University.Controllers
                 return NotFound();
             }
 
-            return View(student);
+            return View(vm);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //POST: Student/Create
+        //see meetod salvestab uue studenti andmebaasi
+        [HttpPost]
+        //see meetod on kaitstud CSRF rünnakute eest
+        //see meetod on asünkroonene, mis tähendab, et see meetod ei saa
+        //olla samaaegselt mitu korda käivitatud
+        public async Task<IActionResult> Create(StudentCreateViewModel vm)
+        {
+            //kui model on valiidne, siis loome uue studenti ja salvestame selle andmebaasi
+            if (ModelState.IsValid)
+            {
+                var student = new Models.Student
+                {
+                    LastName = vm.LastName,
+                    FirstMidName = vm.FirstMidName,
+                    EnrollmentDate = vm.EnrollmentDate
+                };
+                //lisame studenti andmebaasi ja salvestame muudatused
+                _context.Add(student);
+                //miks kasutame await?
+                //kui me kasutame await, siis me ootame kuni salvestamine on lõpetatud
+                await _context.SaveChangesAsync();
+                //pärast salvestamist suuname kasutaja tagasi Index vaatesse
+                return RedirectToAction(nameof(Index));
+            }
+            return View(vm);
         }
     }
 }
---
+++ 12. 01.04.26/University/University/ViewModel/StudentCreateViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace University.ViewModel
+{
+    public class StudentCreateViewModel
+    {
+        public int Id { get; set; }
+        public string LastName { get; set; }
+        public string FirstMidName { get; set; }
+        public DateTime EnrollmentDate { get; set; }
+    }
+}
