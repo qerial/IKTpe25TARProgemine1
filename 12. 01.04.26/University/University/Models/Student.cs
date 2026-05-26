dd10c9133675ce4761c97a41653f67789e9e0ec2
Opilane TTHK <opilane@tthk.ee>
2026-05-26 14:55:46 +0300

    Added new migration and updated database + 1 table

--- 12. 01.04.26/University/University/Models/Student.cs
+++ 12. 01.04.26/University/University/Models/Student.cs
@@ -12,6 +12,9 @@ namespace University.Models
 
         public string FirstMidName { get; set; }
 
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+
         public DateTime EnrollmentDate { get; set; }
 
         public ICollection<Enrollment> Enrollments { get; set; }
