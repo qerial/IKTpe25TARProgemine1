12b9a6ee5033bdf5074d6b60424a7e7cbf98c1c9
qerial <avigolainen@gmail.com>
2026-06-11 11:47:30 +0300

    Enhance FileToApi model with additional properties

--- 12. 01.04.26/University/University/Models/FileToApi.cs
+++ 12. 01.04.26/University/University/Models/FileToApi.cs
@@ -1,9 +1,16 @@
-﻿namespace University.Models
+namespace University.Models
 {
     public class FileToApi
     {
         public Guid Id { get; set; }
+        public string? FileName { get; set; }
         public string? ExistingFilePath { get; set; }
+        public long FileSize { get; set; }
+        public string? ContentType { get; set; }
         public int CourseId { get; set; }
+        public DateTime UploadedDate { get; set; }
+
+        // Foreign key
+        public Course? Course { get; set; }
     }
 }
